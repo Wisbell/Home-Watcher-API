d95e695987a88784d39dae5b23dc835b5512e375
Wisbell <westleyisbell@gmail.com>
2017-05-09 11:07:24 -0500

    Added model schema to model.js in models folder

---
+++ models/models.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const Media = mongoose.model('media', mediaSchema)
+const User = mongoose.model('user', mediaSchema)
+
+const mediaSchema = {
+  dateCreated: String,
+  mediaType: String,
+  url: String
+}
+
+const userSchema = {
+  dateCreated: String,
+  email: String,
+  password: String
+}
